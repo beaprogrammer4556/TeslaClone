5a283a460ef909ed2739f1f88e235d5d6fb655b6
beaprogrammer4556 <86396054+beaprogrammer4556@users.noreply.github.com>
2021-08-21 22:54:51 +0530

    CODE ADDED

---
+++ src/components/Home.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import styled from "styled-components";
+import Section from './Section';
+function Home() {
+    return (
+        <Container>
+
+            <Section
+                title="Model S"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-s.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model Y"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing Inventory"
+            
+            />
+            <Section
+                title="Model 3"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-3.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing Inventory"
+            
+            />
+            <Section
+                title="Model X"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing Inventory"
+            
+            />
+            <Section
+                title="Solar Panel"
+                description="Lowest Cost Solar Panels in America"                
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="ORDER NOW"
+                rightBtnText="LEARN MORE"
+            
+            />
+            <Section
+                title="Solar Roof"
+                description="Produce Clean Energy From Your Roof"
+                backgroundImg="solar-roof.jpg"
+                leftBtnText="ORDER NOW"
+                rightBtnText="LEARN MORE"
+            
+            />
+            <Section
+                title="Accessories"                
+                description=""
+                backgroundImg="accessories.jpg"
+                leftBtnText="SHOP NOW"
+            
+            />
+        </Container>
+    )
+}
+
+export default Home
+
+const Container = styled.div`
+    height:100vh;
+
+`
